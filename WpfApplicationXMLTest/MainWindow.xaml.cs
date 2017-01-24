910b2b41d479b6f660cdcc864f9f91cc0468d741
Сергей Черкасских <6223726@mail.ru>
2017-01-24 23:12:43 +0300

    ss

--- WpfApplicationXMLTest/MainWindow.xaml.cs
+++ WpfApplicationXMLTest/MainWindow.xaml.cs
@@ -39,7 +39,6 @@ namespace Rar
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                
                 XDocument xdoc = XDocument.Load(openFileDialog.FileName);
                 XElement root = xdoc.Root;
 
@@ -48,47 +47,27 @@ namespace Rar
                 File.DocumentDate  =  DateTime.Parse(root.Attribute("ДатаДок").Value);
 
                 XElement references = root.Element("Справочники");
-                //XElement partners = root.Element("Справочники").Element("Контрагенты");
-
-                //XNode partner = references.FirstNode;
                 foreach (XNode node in references.Elements("Контрагенты"))
                 {
                     RarCompany rc = new RarCompany();
                     XElement el = (XElement)node;
                     rc.Name = el.Attribute("П000000000007").Value;
+                    rc.ID = el.Attribute("ИдКонтр").Value;
+                    XElement resident = el.Element("Резидент");
+                    if (resident!=null)
+                    {
+                        XElement corporateBody = resident.Element("ЮЛ");
+                        if (corporateBody != null)
+                        {
+                            rc.INN = corporateBody.Attribute("П000000000009").Value;
+                            rc.KPP = corporateBody.Attribute("П000000000010").Value;
+                        }
+                    }
                 }
-
-                //while (partner != null)
-                //{
-                //    //проверяем, что текущий узел - это элемент
-                //    if (partner.NodeType == System.Xml.XmlNodeType.Element)
-                //    {
-                //        XElement el = (XElement)partner;
-                //        RarCompany rc = new RarCompany();
-
-                //        if (el.Name == "Контрагенты")
-                //        {
-                //            rc.Name = el.Attribute("П000000000007").Value;
-                //        }
-                //        else
-                //        {
-                //            rc.Name = el.Attribute("П000000000004").Value;
-                //            rc.Producter = true;
-                //        }
-
-                //        CompanyList.Add(rc);
-                //        //dataGridCompanies.Items.Add(rc);
-                //    }
-                //    partner = partner.NextNode;
-                //}
             }
             IList<RarCompany> rac = CompanyList.Select(p => p).ToList();
             dataGridCompanies.ItemsSource = rac;
-            //foreach (RarCompany item in rac)
-            //{
-            //    dataGridCompanies.Items.Add(item);
 
-            //}
         }
     }
 }
